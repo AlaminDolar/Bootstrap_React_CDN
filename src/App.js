4b9cdfce29f61ad001c2cea45f5069e40ffa10a8
ALAMIN Dolar <you@example.com>
2021-10-07 21:41:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
+  const items = [
+    {name:'Fisrst Item', description:'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'},
+    {name:'Fisrst Item', description: 'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'},{name:'Fisrst Item', description: 'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'},{name:'Fisrst Item', description: 'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'},{name:'Fisrst Item', description: 'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'},{name:'Fisrst Item', description: 'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'},{name:'Fisrst Item', description: 'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'},{name:'Fisrst Item', description: 'I am a Muslim', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQeIfhG0ykS8r_GH0Lr1ozPmSn8ua3wLgPQ&usqp=CAU'}  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="row row-cols-1 row-cols-md-4 g-3">
+   {
+     items.map(item => <Card
+     item = {item} >
+     </Card>)
+   }
+  </div>
   );
 }
 
